349a5f3a6f05457648a81e7b5dfba9a87f6f3ec3
tabeyla <mgan944@gmail.com>
2019-12-05 14:35:41 -0500

    update container registry

--- src/main/java/com/mn/demo/azure/devop/service/DevopController.java
+++ src/main/java/com/mn/demo/azure/devop/service/DevopController.java
@@ -7,6 +7,10 @@ import io.micronaut.http.annotation.Get;
 @Controller
 public class DevopController {
 
+    /**
+     * Add a controller
+     * @return
+     */
     @Get("/hello")
     public String hello() {
         return "hello azure devops to a new docker registry again";
